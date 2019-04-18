90808eac8ba6083a832d4144dba7456575455ccc
eyelyn <eyelyn@g1project.net>
2019-04-18 13:48:58 +0900

    Eyelyn Test2

--- practice/src/main/java/eyelyn/EyelynTest.java
+++ practice/src/main/java/eyelyn/EyelynTest.java
@@ -3,7 +3,7 @@ package eyelyn;
 public class EyelynTest {
 	
 	public static void main(String[] args) {
-        System.out.println("Eyelyn Hello world");
+        System.out.println("Eyelyn Hello world!!!");
     }
 
 }
