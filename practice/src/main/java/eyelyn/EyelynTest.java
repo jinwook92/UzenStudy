7b3d266ea463d9fef0edc40baeb138960abfd0c6
eyelyn <eyelyn@g1project.net>
2019-04-25 10:43:52 +0900

    eyelyn git commit test

--- practice/src/main/java/eyelyn/EyelynTest.java
+++ practice/src/main/java/eyelyn/EyelynTest.java
@@ -3,6 +3,7 @@ package eyelyn;
 public class EyelynTest {
 	
 	public static void main(String[] args) {
+		System.out.println("pull Request");
         System.out.println("Eyelyn Hello world!!!");
     }
 
