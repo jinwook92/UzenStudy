6f8a66505a13023dbf31183994480766a2abca7e
way <j99hg99@gmail.com>
2019-04-26 13:49:11 +0900

    자료구조 파트1,2 -ArrayList

--- practice/src/main/java/way/WayArrayList.java
+++ practice/src/main/java/way/WayArrayList.java
@@ -7,25 +7,26 @@ import java.util.List;
 public class WayTest {
 
     public static void main(String[] args) {
-        //1. List import
+        //1. ArrayList - JAVA API 사용해보기(1) - List import
 
-        //2. String ArrayList Declare
+        //2. ArrayList - JAVA API 사용해보기(2) String ArrayList
         List<String> my_arr_list  = new ArrayList<>();
 
-        //3.add
+        //3. ArrayList - JAVA API 사용해보기(3) add
         my_arr_list.add("Hello");
         my_arr_list.add("Java");
         my_arr_list.add("World!!");
 
-        //4. delete
+        //4. ArrayList - JAVA API 사용해보기(4) delete
         my_arr_list.remove(2);
 
-        //5. get
+        //5. ArrayList - JAVA API 사용해보기(5) get
         System.out.println(my_arr_list.get(1));
 
-        //6. Iterator
+        //6. JAVA API 반복문 쓰기(1) - Iterator
         Iterator<String> it = my_arr_list.iterator();
 
+        //7. ArrayList - JAVA API 반복문 쓰기(2)
         while(it.hasNext()){
             System.out.println(it.next());
         }
