e876e48ff9007792bd7137249e294eb9841474ba
eyelyn <eyelyn@g1project.net>
2019-05-09 15:57:55 +0900

    week2 - 자바로 배우는 자료구조(with 생활코딩) 과제

---
+++ practice/src/main/java/eyelyn/datastructure2/MainRunner.java
@@ -0,0 +1,17 @@
+package eyelyn.datastructure2;
+
+//실행을 위한 코드입니다. 수정하지 마세요.
+public class MainRunner 
+{
+ public static void main(String[] args)
+ {
+     ArrayList arraylist = new ArrayList();
+     for(int i=0; i<1000; i++){
+         arraylist.addLast(i);
+     }
+     
+     System.out.println(arraylist.output());
+     
+     return ;
+ }
+}
